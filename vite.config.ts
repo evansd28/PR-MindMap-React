4abbe146b4bd874caf87cf1efce32d7cf876ce0a
Daniel Evans <dae69@pitt.edu>
2025-04-07 19:13:22 -0400

    change base url in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PR-MindMap-React/'
+  base: 'https://pr-mind-map-react-smqe.vercel.app/'
 })
